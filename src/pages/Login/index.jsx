057019ab639eae8422eea071ce50764b771978c6
Heekyung Do <dhk0729@gmail.com>
2023-11-24 00:13:12 +0900

    로그인 & 회원가입 ui

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -4,17 +4,23 @@ import styles from "./login.module.css";
 const Login = () => {
   return (
     <section>
-      <div>
+      <div className={styles.login}>
         <h3>로그인</h3>
-        <form>
-          <div>
-            <label htmlFor="">아이디</label>
-            <input type="text" />
+        <form className={styles.form}>
+          <div className={styles.inputBox}>
+            {/* <label htmlFor="">아이디</label> */}
+            <input type="text" placeholder="아이디" />
           </div>
-          <div>
-            <label htmlFor="">비밀번호</label>
-            <input type="text" />
+          <div className={styles.inputBox}>
+            {/* <label htmlFor="">비밀번호</label> */}
+            <input type="text" placeholder="비밀번호" />
           </div>
+          <div className={styles.button}>
+            <button type="submit">로그인</button>
+          </div>
+          <p className={styles.goRegister}>
+            회원이 아니라면? <a href="/register">회원가입</a>
+          </p>
         </form>
       </div>
     </section>
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -4,21 +4,27 @@ import styles from "./register.module.css";
 const Register = () => {
   return (
     <section>
-      <div>
+      <div className={styles.register}>
         <h3>회원가입</h3>
-        <form>
-          <div>
-            <label htmlFor="">아이디</label>
-            <input type="text" />
+        <form className={styles.form}>
+          <div className={styles.inputBox}>
+            {/* <label htmlFor="">아이디</label> */}
+            <input type="text" placeholder="아이디" />
           </div>
-          <div>
-            <label htmlFor="">이름</label>
-            <input type="text" />
+          <div className={styles.inputBox}>
+            {/* <label htmlFor="">이름</label> */}
+            <input type="text" placeholder="이름" />
           </div>
-          <div>
-            <label htmlFor="">비밀번호</label>
-            <input type="text" />
+          <div className={styles.inputBox}>
+            {/* <label htmlFor="">비밀번호</label> */}
+            <input type="text" placeholder="비밀번호" />
           </div>
+          <div className={styles.button}>
+            <button type="submit">회원가입</button>
+          </div>
+          <p className={styles.goLogin}>
+            아이디가 있다면? <a href="/login">로그인</a>
+          </p>
         </form>
       </div>
     </section>
