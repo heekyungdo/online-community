f419fa51317680ec1ea502d82d543e7342dd9fa1
Clara <dhk0729@gmail.com>
2024-01-09 19:07:22 +0900

    에러 수정

--- src/components/CommentInput.jsx
+++ src/components/CommentInput.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 
 const CommentTitle = styled.p`
